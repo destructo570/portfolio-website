54bcba215199c2fb6e97ab22e67b932b3a4bceb7
Vishal Kashi <dev.vishalkashi@gmail.com>
2021-10-25 20:51:34 +0530

    Refactor projects section

---
+++ src/components/Projects/ProjectItems.js
@@ -0,0 +1,26 @@
+import "./ProjectItems.css";
+import ProjectTechStack from "./ProjectTechStack";
+import ProjectLinks from "./ProjectLinks";
+function ProjectItems(props) {
+  return (
+    <div>
+      {props.projects.map((project, index) => {
+        return (
+          <div key={index} className={project.cName}>
+            <div className="project__img">
+              <img src={project.imgUrl} alt=""></img>
+            </div>
+            <div className="project__content">
+              <h2 className="project-title">{project.title}</h2>
+              <p className="project-description">{project.description}</p>
+              <ProjectTechStack techStack={project.techStack} />
+              <ProjectLinks links={project.projectLinks} />
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default ProjectItems;
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,28 +1,11 @@
 import "./Projects.css";
 import { ProjectList } from "./ProjectList";
-import ProjectTechStack from "./ProjectTechStack";
-import ProjectLinks from "./ProjectLinks";
+import ProjectItems from "./ProjectItems";
 function Projects() {
   return (
     <section className="projects">
       <h1>Projects</h1>
-      <div>
-        {ProjectList.map((project, index) => {
-          return (
-            <div key={index} className={project.cName}>
-              <div className="project__img">
-                <img src={project.imgUrl} alt=""></img>
-              </div>
-              <div className="project__content">
-                <h2 className="project-title">{project.title}</h2>
-                <p className="project-description">{project.description}</p>
-                <ProjectTechStack techStack={project.techStack} />
-                <ProjectLinks links={project.projectLinks} />
-              </div>
-            </div>
-          );
-        })}
-      </div>
+      <ProjectItems projects={ProjectList} />
     </section>
   );
 }
