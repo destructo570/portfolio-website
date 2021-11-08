dfff1b620908eb45d78ac8c39efae829eef9d775
Vishal Kashi <dev.vishalkashi@gmail.com>
2021-11-08 19:40:43 +0530

    Use styled components in footer section

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,18 @@
-import "./Footer.css";
+import styled from "styled-components";
+
+const FooterSection = styled.footer`
+  display: flex;
+  justify-content: center;
+  font-size: 0.8rem;
+  padding: 1.2rem 0;
+  font-family: "Noto Sans Mono", serif;
+`;
 
 function Footer() {
   return (
-    <footer>
+    <FooterSection>
       <p>Designed and built by Vishal Kashi.</p>
-    </footer>
+    </FooterSection>
   );
 }
 
