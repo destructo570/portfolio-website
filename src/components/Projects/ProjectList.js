85af1200f551ff04bdc293d0b820c1c69fa28f89
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-09-19 15:12:33 +0530

    Update ProjectList.js

--- src/components/Projects/ProjectList.js
+++ src/components/Projects/ProjectList.js
@@ -1,6 +1,88 @@
 export const ProjectList = [
   {
     id: 1,
+    title: "Kanban Board MERN Web App",
+    description: `A minimal kanban board web app made with MERN stack. Manage all your projects and tasks at one place.
+    Create, Update and Delete boards, cards and lists to manage your projects. Backend REST Api created with Node and Express
+    with JWT authentication, server side validation and error handling.
+    `,
+    imgUrl: "https://imgur.com/z5GvyXL.png",
+    techStack: [
+      "React",
+      "Redux",
+      "NodeJs",
+      "Express",
+      "MongoDb",
+      "Jwt Auth",
+      "Mongoose",
+      "Axios",
+    ],
+    projectLinks: [
+      {
+        iconClassName: "ri-github-line ri-xl",
+        url: "https://github.com/destructo570/kanban-board-mern-stack",
+      },
+      {
+        iconClassName: "ri-link-m ri-xl",
+        url: "https://digital-kanban-board-react.vercel.app/home",
+      },
+    ],
+    cName: "project-item",
+  },
+  {
+    id: 2,
+    title: "Deplace Maison Ecommerce Webapp ",
+    description: `A full stack clone of deplace maison ecoomerce website using Next JS with stripe payment integration and firebase for storing orders.`,
+    imgUrl: "https://imgur.com/snvQ2FA.png",
+    techStack: [
+      "NextJs",
+      "Context Api",
+      "Axios",
+      "Firebase",
+      "Stripe",
+      "Next Auth",
+      "Axios",
+    ],
+    projectLinks: [
+      {
+        iconClassName: "ri-github-line ri-xl",
+        url: "https://github.com/destructo570/deplace-maison-ecommerce-webapp",
+      },
+      {
+        iconClassName: "ri-link-m ri-xl",
+        url: "https://deplace-maison-ecommerce-webapp-951b22zlw-destructo570.vercel.app/",
+      },
+    ],
+    cName: "project-item",
+  },
+  {
+    id: 3,
+    title: "React Countries App",
+    description: `See useful information related to all countries like currency, native names, border countries etc.
+    We are using REST Countries API for getting data related to a country.`,
+    imgUrl: "https://imgur.com/mLTg655.png",
+    techStack: [
+      "ReactJS",
+      "React router",
+      "Axios",
+      "HTML",
+      "CSS",
+      "Javascript",
+    ],
+    projectLinks: [
+      {
+        iconClassName: "ri-github-line ri-xl",
+        url: "https://github.com/destructo570/react-countries",
+      },
+      {
+        iconClassName: "ri-link-m ri-xl",
+        url: "https://react-countries-azure.vercel.app/countries",
+      },
+    ],
+    cName: "project-item",
+  },
+  {
+    id: 4,
     title: "Sushi : Unofficial myanimelist app",
     description: `Myanimelist website had a very poor design and no decent app for it. I decided to solve this problem by creating Sushi.
     App was downloaded over 20K+ times with 10K+ daily active users and received very good ratings for overall design and functionality of the app.
@@ -20,7 +102,7 @@ export const ProjectList = [
     cName: "project-item",
   },
   {
-    id: 2,
+    id: 5,
     title: "Mars App",
     description:
       "App uses NASA's API to show you the latest photos taken by all the mars rovers like curiosity, perseverance etc.",
@@ -39,7 +121,7 @@ export const ProjectList = [
     cName: "project-item",
   },
   {
-    id: 3,
+    id: 6,
     title: "Covid Tracker",
     description:
       "A minimally designed app to track current statistics of COVID-19 across the world and detailed state wise statistics of India. App also offers some useful information regarding COVID-19, along with helpline numbers.",
@@ -54,7 +136,7 @@ export const ProjectList = [
     cName: "project-item",
   },
   {
-    id: 4,
+    id: 7,
     title: "Erza Icon Pack",
     description:
       "I was inspired by Google's material design and wanted to use all my design skills to create something unique. Erza is a material design inspired icon pack for giving your android phone a fresh and unique look.",
@@ -69,7 +151,7 @@ export const ProjectList = [
     cName: "project-item",
   },
   {
-    id: 5,
+    id: 8,
     title: "Zeref Icon Pack",
     description:
       "Another icon pack I had alot of fun working on, I wanted to create something funky and vibrant. Zeref is a vibrant and minimalist icon pack for those who are tired of the same old material design icon packs.",
